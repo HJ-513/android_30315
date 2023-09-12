7d1559a01ba0d0aa08811ccda92d93794c424af4
HJ00 <hjchoai1305@gmail.com>
2023-09-12 10:46:53 +0900

    0912

--- Games/app/src/main/java/com/example/games/LottoActivity.java
+++ Games/app/src/main/java/com/example/games/LottoActivity.java
@@ -7,10 +7,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.NumberPicker;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.games.databinding.ActivityLottoBinding;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class LottoActivity extends AppCompatActivity implements NumberPicker.OnScrollListener, View.OnClickListener {
 
@@ -60,11 +62,99 @@ public class LottoActivity extends AppCompatActivity implements NumberPicker.OnS
 
     @Override
     public void onScrollStateChange(NumberPicker numberPicker, int i) {
+        if(!binding.switch1.isChecked()) return;
+        if(i == SCROLL_STATE_IDLE) addOneNumber();
+    }
 
+    private void addOneNumber() {
+        if(myNumList.size() == 6){
+            Toast.makeText(getApplicationContext(), "6개를 모두 선택했습니다.", Toast.LENGTH_SHORT).show();
+            binding.numberPicker.setEnabled(false);
+            return;
+        }
+
+        int pickNumber = binding.numberPicker.getValue();
+        TextView tempTextView;
+
+        if(myNumList.contains(pickNumber)){
+            Toast.makeText(getApplicationContext(), "숫자가 중복됩니다.", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            myNumList.add(pickNumber);
+            tempTextView = myNumTextView.get(myNumList.size() - 1);
+            tempTextView.setText(Integer.toString(pickNumber));
+            tempTextView.setVisibility(View.VISIBLE);
+        }
     }
 
     @Override
     public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.addButton:
+                if(binding.switch1.isChecked()) return;
+                addOneNumber();
+                break;
+            case R.id.clearButton:
+                clearNumbers();
+                break;
+            case R.id.runButton:
+                addRandomNumber();
+                winCount();
+                break;
+        }
+    }
 
+    private void winCount() {
+        int count = 0;
+        String msg = "당첨숫자 : ";
+
+        for (Integer myNum : myNumList) {
+            if(lottoNumList.contains(myNum)){
+                count++;
+                msg += myNum + " ";
+            }
+        }
+        binding.txtResult.setText(msg + "\n당첨개수 : " + count);
+    }
+
+    private void addRandomNumber() {
+        // 주작 금지. 사용자 6개 뽑아야 추첨 가능
+        if(myNumList.size() != 6){
+            Toast.makeText(getApplicationContext(), "6개의 숫자를 선택해야 추첨할 수 있습니다.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        // 로또 번호가 6개면 초기화하기
+        if(lottoNumList.size() == 6) lottoNumList.clear();
+
+        int randomNumber = 0;
+        TextView tempTextView;
+        Random r = new Random();
+
+        while(lottoNumList.size() < 6){
+            randomNumber = r.nextInt(45) + 1;
+
+            if(!lottoNumList.contains(randomNumber)){
+                lottoNumList.add(randomNumber);
+                tempTextView = lottoNumTextView.get(lottoNumList.size() - 1);
+                tempTextView.setText(Integer.toString(randomNumber));
+                tempTextView.setVisibility(View.VISIBLE);
+            }
+        }
+    }
+
+    private void clearNumbers() {
+        // TextView들의 visibility 조정
+        // 넘버리스트 비우기
+        for (TextView tv : myNumTextView) {
+            tv.setVisibility(View.GONE);
+        }
+        for (TextView t : lottoNumTextView) {
+            t.setVisibility(View.GONE);
+        }
+        myNumList.clear();
+        lottoNumList.clear();
+
+        binding.numberPicker.setEnabled(true);
+        binding.txtResult.setText("6개의 숫자를 선택하세요");
     }
 }
